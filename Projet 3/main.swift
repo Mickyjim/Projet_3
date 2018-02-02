8cbba5fc6bfe83b74ad2f9edf9ec7e5a74a2699d
Michael Favre <favre_michael@yahoo.fr>
2018-02-02 19:05:41 +0100

    Project initialization

---
+++ Projet 3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projet 3
+//
+//  Created by Michael Favre on 02/02/2018.
+//  Copyright © 2018 Michael Favre. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
